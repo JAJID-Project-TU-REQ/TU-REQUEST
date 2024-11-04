a705916339dcce25a65a2064221f2767404c3300
jeangtpr <thapharan.vit@dome.tu.ac.th>
2024-11-04 21:24:17 +0700

    Remove unused imports in App.js and add .gitignore for node_modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from './page/Home.js';
-import RequestInformation from './page/RequestInformation.js';
-import History from './page/History.js';
-import Allreq  from "./page/Allrequest.js";
-//below this is req page
+
 
 
 function App() {
-  
+
   return (
     <div>
-        
+
     </div>
   );
 }
