1de622c3f8c896f058cd5a55b2e43750b4aa4676
KeeratiSaiwan <keerati.sai@dome.tu.ac.th>
2024-11-04 22:48:41 +0700

    draft 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Siderbar from './components/sidebar'
+import TemporaryDrawer from './components/jeang'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <TemporaryDrawer />
+
+    </div>
   )
 }
 
-export default App
+export default App;
