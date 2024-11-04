8869b955f3ab220bd50b881d2583a9b50dadf412
KeeratiSaiwan <keerati.sai@dome.tu.ac.th>
2024-11-04 22:33:04 +0700

    Co-authored-by: jeangtpr <jeangtpr@users.noreply.github.com> Co-authored-by: Dewnosaur <Dewnosaur@users.noreply.github.com> Co-authored-by: meaom <jinnapat.pen@dome.tu.ac.th> Co-authored-by: Rapeephanwadee Plodpran <rapeephanwadee.plo@dome.tu.ac.th>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,24 @@
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <div>
-        <nav>
-          <div class="container">
-            <div class="nav-con">
-              <div class="logo1">
-                <a href="#"><img src="logo-bar" alt="Logo"/></a>
-              </div>
-            </div>
-          </div>
-        </nav>
-        <div class="login-box">
-          <div class="wrapper">
-            <form action="">
-              <h1>University Request System</h1>
-              <h2>TU REQUEST SYSTEM (TU-REQ)</h2>
-              <div class="input-box">
-                <input type="text" placeholder="ID" required/>
-                <i class='bx bxs-user'></i>
-              </div>
-              <div class="input-box">
-                <input type="password" placeholder="Password" required/>
-                <i class='bx bxs-lock-alt'></i>
-              </div>
-              <button type="submit" class="btn">LOGIN</button>
-              <div class="cond">
-                <h3>For first-time login</h3>
-                <p>Use student ID, password is:</p>
-                <p>TUREQ(last 4 digits of student ID)</p>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
