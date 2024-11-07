7626dd87ffe43c24514a8a9a0c9f85d19c6bf016
Dewnosaur <bhuripat.see@dome.tu.ac.th>
2024-11-07 21:15:09 +0700

    prof dashboard

--- frontend/src/pages/ProfessorDashboard.js
+++ frontend/src/pages/ProfessorDashboard.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Avatar from '@mui/material/Avatar';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { Link } from "react-router-dom";
+
 
 function ProfessorDashboard() {
   return (
-    <div>
-      Hello I'm Professor Dashboard
-    </div>
+    <Container sx={{ p:2 }} maxWidth="lg">    
+      <Paper sx={{ p:2 }}>
+        <Box display="flex">
+          <Box flexGrow={1}>
+            <Typography component="h2" variant="h6" color="primary" gutterBottom>
+              USERS
+            </Typography>
+          </Box>
+          <Box>
+            <Link to="/create">
+              <Button variant="contained" color="primary">
+                CREATE
+              </Button>
+            </Link>
+          </Box>
+        </Box>
+        <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">วันที่</TableCell>
+              <TableCell align="center">ประเภท</TableCell>
+              <TableCell align="center">หัวข้อคำร้อง</TableCell>
+              <TableCell align="center">รายละเอียด</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              <TableRow >
+                <TableCell align="center">today</TableCell>
+                <TableCell align="center">default</TableCell>
+                <TableCell align="center">title</TableCell>
+                <TableCell align="center">
+                  <ButtonGroup color="primary" aria-label="outlined primary button group">
+                    <Button >Edit</Button>
+                  </ButtonGroup>
+                </TableCell>
+              </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </Paper>
+    </Container>
   )
 }
 
